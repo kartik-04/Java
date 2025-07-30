56cad205fb2851959189a28bb91f671ab0113561
kartik-04 <04kartik04@gmail.com>
2025-07-30 23:55:06 +0530

    Tying to get concurrency

--- src/concurrency/Sorter.java
+++ src/concurrency/Sorter.java
@@ -1,4 +1,11 @@
 package concurrency;
 
-public class Sorter {
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class Sorter implements Callable<List<Integer>> {
+    @Override
+    public List<Integer> call() throws Exception {
+        return List.of();
+    }
 }
